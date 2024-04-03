6e4227427ec9d6cad8854a4e3d925aa755b0604f
garciaerin <egarcia0715@conestogac.on.ca>
2024-04-03 17:07:38 -0400

    finished the video 'Update Post with Ajax'

--- posts/views.py
+++ posts/views.py
@@ -93,14 +93,14 @@ def update_post(request, pk):
         obj.title = new_title
         obj.body = new_body
         obj.save()
-    return JsonResponse({
-        'title': new_title,
-        'body': new_body,
-    })
+        return JsonResponse({
+            'title': new_title,
+            'body': new_body,
+        })
 
 
 def delete_post(request, pk):
     obj = Post.objects.get(pk=pk)
-    if request.is_ajax():
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         obj.delete()
-    return JsonResponse({})
+        return JsonResponse({})
